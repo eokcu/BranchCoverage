fd6b50955584921ddb0b60ff7544e101172660c6
Esat Okcu <esatokcu216@gmail.com>
2024-03-30 22:07:04 +0300

    Added new tests in TestUtil.java

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -12,5 +12,23 @@ public class TestUtil {
   public void setUp() { c = new Util(); }
 
   @Test
-  public void example() { assertTrue(true); }
+  public void lengthOne() {
+    assertFalse(c.compute(1));
+  }
+  @Test
+  public void lengthEven(){
+    assertFalse(c.compute(1, 2, 3, 4));
+  }
+  @Test(expected = RuntimeException.class)
+  public void zeroValue(){
+    c.compute(0, 1, 2);
+  }
+  @Test
+  public void oddValues(){
+    assertTrue(c.compute(1, 3, 5));
+  }
+  @Test
+  public void primeValues(){
+    assertFalse(c.compute(7, 11, 13));
+  }
 }
