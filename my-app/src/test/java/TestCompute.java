def526f56dcad2b5a02649d67e8eefc6c77e69bc
Esat Okcu <esatokcu216@gmail.com>
2024-03-30 22:30:38 +0300

    Added new tests in TestCompute.java

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -9,9 +9,30 @@ public class TestCompute {
   Compute c;
 
   @Test
-  public void example() {
+  public void zeroMQSize() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
-    assertTrue(true);
+    String e = "hello";
+    when(mq.size()).thenReturn(0);
+    assertEquals(-1, c.countNumberOfOccurrences(e));
+  }
+  @Test
+  public void notIncludingOccurences(){
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    String e = "hello";
+    when(mq.size()).thenReturn(1);
+    when(mq.contains(e)).thenReturn(false);
+    assertEquals(0, c.countNumberOfOccurrences(e));
+  }
+  @Test
+  public void countOneOccurence(){
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    String e = "hello";
+    when(mq.size()).thenReturn(2);
+    when(mq.contains(e)).thenReturn(true);
+    when(mq.getAt(0)).thenReturn(e);
+    assertEquals(1, c.countNumberOfOccurrences(e));
   }
 }
